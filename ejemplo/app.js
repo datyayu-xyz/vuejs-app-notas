1e18ee9bb3375c934cea2a1155f211b63459c311
Arturo Coronel <aoitsu3@gmail.com>
2017-01-29 04:51:22 -0800

    Repo creado. Ejemplo v1

---
+++ ejemplo/app.js
@@ -0,0 +1,61 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        titulo: '',
+        texto: '',
+        notas: [],
+        notaActivaId: null,
+        creandoNota: false,
+        mostrandoNota: false,
+    },
+
+    computed: {
+        notaActiva: function() {
+            var vm = this;
+
+            return vm.notas.find(function(nota) {
+                return nota.id === vm.notaActivaId;
+            })
+        }
+    },
+
+    methods: {
+        crearNota: function() {
+            this.titulo = '';
+            this.texto = '';
+            this.creandoNota = true;
+        },
+
+        guardarNota: function() {
+            if (!this.titulo || !this.texto) {
+                alert('La nota requiere de un titulo y contenido!')
+                return;
+            }
+
+            var nota = {
+                id: Date.now(),
+                titulo: this.titulo,
+                texto: this.texto,
+            };
+
+            this.notas.push(nota);
+            this.creandoNota = false;
+        },
+
+        cancelarNota: function() {
+            this.areaActiva = '';
+            this.creandoNota = false;
+        },
+
+        mostrarNota: function(id) {
+            this.notaActivaId = id;
+            this.mostrandoNota = true;
+        },
+
+        ocultarNota: function() {
+            this.notaActivaId = null;
+            this.mostrandoNota = false;
+        },
+    },
+});
